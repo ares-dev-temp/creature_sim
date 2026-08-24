17d9898799a291b2f5b27279f220b2035582e1ee
ares-dev-temp <jisairc54@gmail.com>
2026-08-23 23:32:11 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+mkdir -p build
+
+clang -c glad.c -I./include -o glad.o
+
+clang++ main.cpp glad.o \
+-I./include \
+-I/opt/homebrew/include \
+-L/opt/homebrew/lib \
+-lglfw \
+-lcjson \
+-framework Cocoa \
+-framework OpenGL \
+-framework IOKit \
+-framework CoreVideo \
+-o build/app
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int screenWidth = 854, screenHeight = 480;
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    // Request core profile for modern OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on macOS
+
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // 5. Load OpenGL functions with GLAD
+    int version = gladLoadGL();
+
+    if (version == 0)
+    {
+        std::cerr << "Failed to initialize GLAD\n";
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "OpenGL version: "
+              << glGetString(GL_VERSION)
+              << '\n';
+
+    // Main render loop
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.4f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
